3dc35cc0650a80639ab5005730c3eaf7d05044b0
ChrisKim <chriskim10@live.co.kr>
2020-10-02 01:59:17 +0900

    메뉴 리스트 추가

--- Kiosk/MainWindow.xaml.cs
+++ Kiosk/MainWindow.xaml.cs
@@ -20,11 +20,35 @@ namespace Kiosk
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<Food> lstFood = new List<Food>()
+        {
+            new Food(){ category = Category.SPECIAL, name = "기네스머쉬룸와퍼", imagePath = @"/Assets/기네스머쉬룸와퍼.png" },
+            new Food(){ category = Category.SPECIAL, name = "기네스와퍼", imagePath = @"/Assets/기네스와퍼.png" },
+            new Food(){ category = Category.SPECIAL, name = "기네스콰트로치즈와퍼", imagePath = @"/Assets/기네스콰트로치즈와퍼.png" },
+
+            new Food(){ category = Category.PRIMIUM, name = "기네스와퍼팩1", imagePath = @"/Assets/기네스와퍼팩1.png" },
+            new Food(){ category = Category.PRIMIUM, name = "기네스와퍼팩2", imagePath = @"/Assets/기네스와퍼팩2.png" },
+            new Food(){ category = Category.PRIMIUM, name = "기네스와퍼팩3", imagePath = @"/Assets/기네스와퍼팩3.png" },
+
+            new Food(){ category = Category.WAPPER, name = "와퍼", imagePath = @"/Assets/와퍼.png" },
+            new Food(){ category = Category.WAPPER, name = "불고기와퍼", imagePath = @"/Assets/불고기와퍼.png" },
+            new Food(){ category = Category.WAPPER, name = "치즈와퍼", imagePath = @"/Assets/치즈와퍼.png" },
+        };
+
         public MainWindow()
         {
             InitializeComponent();
             Uri fontUri = new Uri(AppDomain.CurrentDomain.BaseDirectory + @"\Fonts\BMDOHYEON_ttf.ttf");
             this.FontFamily = new System.Windows.Media.FontFamily(fontUri, "배달의민족 도현");
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            lbMenus.ItemsSource = lstFood.Where(x => x.category == Category.SPECIAL).ToList();
+
         }
     }
 }
