10e2e2827593059f6d27368f5056623cc432ab14
ChrisKim <ChrisKim10@live.co.kr>
2020-10-04 16:04:05 +0900

    page 이동처리 샘플 추가

---
+++ WPFPageSample/View/OrderPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFPageSample.View
+{
+    /// <summary>
+    /// Interaction logic for OrderPage.xaml
+    /// </summary>
+    public partial class OrderPage : Page
+    {
+        public OrderPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            if(NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack();
+            }
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoForward)
+            {
+                NavigationService.GoForward();
+            }
+            else
+            {
+                PlacePage placePage = new PlacePage();
+                NavigationService.Navigate(placePage);
+            }
+        }
+    }
+}
