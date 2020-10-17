71e4ec4d96db99963ff1a397e137595476c979e0
ChrisKim <ChrisKim10@live.co.kr>
2020-10-17 17:26:57 +0900

    QR코드 인식 샘플

---
+++ QRSample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QRSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //카메라가 여러대일경우 값을 바꾸면 됨.
+            //콤보박스로 제공하면 좋으나 우리 프로젝트에서는 불필요함
+            webcam.CameraIndex = 0;
+        }
+
+        private void webcam_QrDecoded(object sender, string e)
+        {
+            tbRecog.Text = e;
+        }
+    }
+}
